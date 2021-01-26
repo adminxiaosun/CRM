b63abb189520f8f01afcf9cde94fa98b624ffc48
xiaosun <3330779476@qq.com>
2021-01-26 16:36:50 +0800

    第四次提交 修改Admin.java文件

--- Admin.java
+++ Admin.java
@@ -3,6 +3,7 @@ public class Admin{
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("小孙今天使用了git工具");
+		System.out.println("小孙今天使用了git工具, 使用很不顺利");
 	}
 	
 }
